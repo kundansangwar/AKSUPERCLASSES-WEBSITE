a828f83bbeb69e4944bc7782844c1fb793315ef8
AKSUPERCLASSES <aksuperclasses@local>
2026-05-20 09:23:15 +0530

    Wire up Firebase config for ak-super-classes project

--- firebase-config.js
+++ firebase-config.js
@@ -14,10 +14,10 @@
 // =====================================================================
 
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCzHhgKNzmegDXjFjNS3dd1x3uFCq8TXqY",
+    authDomain: "ak-super-classes.firebaseapp.com",
+    projectId: "ak-super-classes",
+    storageBucket: "ak-super-classes.firebasestorage.app",
+    messagingSenderId: "717024706012",
+    appId: "1:717024706012:web:bf18fe99550a31f7a1e319"
 };
